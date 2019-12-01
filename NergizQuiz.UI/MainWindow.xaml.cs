3a0211cae5daf7f6e6643f64b11cb4f3abd8910c
kidproko22223 <kidproko22223@gmail.com>
2019-12-01 13:47:55 +0700

    Change interface and database

---
+++ NergizQuiz.UI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NergizQuiz.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
